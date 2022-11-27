458e69b6909e7be1281bfd11e4cca4f3cb98eb9a
simonholmes001 <simonholmesabc@gmail.com>
2022-11-27 16:41:15 +0100

    [Feature]: code banner section

--- next-client/pages/index.tsx
+++ next-client/pages/index.tsx
@@ -1,8 +1,16 @@
-import Head from "next/head";
-import Image from "next/image";
+import { Banner } from "../components";
 
-const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+const Home = () => (
+<div className="flex justify-center sm:px-4 p-12">
+    <div className="w-full minmd:w-4/5">
+      <Banner
+        banner="Discover, collect and sell extraordinary NFTs on the Cardano Blockchain"
+        childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
+        parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+      />
+    </div>
+  </div>
+);
+  
 
 export default Home;
